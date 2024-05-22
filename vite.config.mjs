7f7dfa725865845ccdd861d688fac5887fe714a4
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-05-22 14:48:40 +0200

    chore: add and configure project settings and dependencies

---
+++ vite.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  let base = '/';
+
+  return {
+    base,
+    root: path.resolve(__dirname),
+    resolve: {
+      alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        '~modern-normalize': path.resolve(__dirname, 'node_modules/modern-normalize'),
+        '~aos': path.resolve(__dirname, 'node_modules/aos'),
+      },
+    },
+    server: {
+      port: 8080,
+      hot: true,
+    },
+    build: {
+      outDir: path.resolve(__dirname, 'dist'),
+    },
+  };
+});
